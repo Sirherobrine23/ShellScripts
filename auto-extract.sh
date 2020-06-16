0de00d4e5524325a4d36015eea9260d54ddf3fda
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-06-16 09:02:21 -0700

    Update auto-extract.sh

--- auto-extract.sh
+++ auto-extract.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 FILES="$(cat ../name.txt)"
 case $FILES in
-  *.zip* ) 
+  *.zip ) 
     sudo apt install -y unzip zip
     sudo unzip "$FILES" 
   ;;
-  *.7z* )
+  *.7z )
     sudo apt install -y p7zip
     sudo 7z -y x "$FILES"
   ;;
-  *.tar.xz* )
+  *.tar.xz )
     sudo apt install -y xz-utils
     sudo tar -xvf "$(cat ../mame.txt)"
+  ;;
+  *.iso )
+    sudo cp "$(cat ../mame.txt)" "$(cat ../mame.txt)_2"
   ;;
 esac
