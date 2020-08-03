92be1540f36f3065ce2bc0610977e230f954d199
matheus S Queiroga <srherobrine20@gmail.com>
2020-08-02 23:14:23 -0300

    update

--- openwrt-internet-status.sh
+++ openwrt-internet-status.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
+# recomendo você deixar esse script em segundo plano no *nix com "/usr/sbin/status &""
 LED="/sys/class/leds/tp-link:green:qss/brightness"
 LED2="/dev/null"
+URL="http://files.sirherobrine23.org"
 update_led="10s"
 # Iniciando o loop
 while [ true ]; do 
-# checando a conexão com a interne (http do google.com.br)
-wget -q -O internet-status --spider http://google.com.br
-
-if [[ $? -eq 0 ]]; then
-  # se haver internet ligara o led
+  for counter in {1..15};do 
+    sleep 0.2s
+    echo "0" > $LED
+    echo "1" > $LED2
+    sleep 0.1s
+    echo "1" > $LED
+    echo "0" > $LED2
+  done
+wget -q -O tested --spider "$URL"
+if [ $? -eq 0 ]; then
   echo "1" > $LED
   echo "0" > $LED2
 else
-  # se não haver internet desligara o led
   echo "0" > $LED
   echo "1" > $LED2
 fi
